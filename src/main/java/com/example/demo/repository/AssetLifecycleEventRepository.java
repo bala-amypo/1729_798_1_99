eacb8e531a4c24990c45f8749c9894b2768dc341
bala-amypo <bala@amypo.in>
2025-12-18 06:43:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetLifecycleEventRepository.java
+++ src/main/java/com/example/demo/repository/AssetLifecycleEventRepository.java
@@ -1,11 +1,9 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
 package com.example.demo.repository;
+
 import com.example.demo.entity.AssetLifecycleEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AssetLifecycleEventRepository 
-        extends JpaRepository<AssetLifecycleEvent, Long> {
-
-    List<AssetLifecycleEvent> findByAssetId(Long assetId);
+@Repository
+public interface AssetLifecycleEventRepository extends JpaRepository<AssetLifecycleEvent, Long> {
 }
