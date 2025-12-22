52b0b3cb05a55a58d31bd7a1c74d75d1a0fd4a1f
bala-amypo <bala@amypo.in>
2025-12-22 09:30:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AssetDisposalServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AssetDisposalServiceImpl.java
@@ -21,13 +21,13 @@ public class AssetDisposalServiceImpl implements AssetDisposalService {
     }
 
     public AssetDisposal requestDisposal(Long assetId, AssetDisposal disposal) {
-        disposal.setAsset(assetRepo.findById(assetId).orElseThrow(() -> new ResourceNotFoundException("Event not found"));
+        disposal.setAsset(assetRepo.findById(assetId).orElseThrow(() -> new ResourceNotFoundException("Event not found")));
         return disposalRepo.save(disposal);
     }
 
     public AssetDisposal approveDisposal(Long disposalId, Long adminId) {
         AssetDisposal disposal = disposalRepo.findById(disposalId).orElseThrow(() -> new ResourceNotFoundException("Event not found"));
-        disposal.setApprovedBy(userRepo.findById(adminId).orElseThrow(() -> new ResourceNotFoundException("Event not found"));
+        disposal.setApprovedBy(userRepo.findById(adminId).orElseThrow(() -> new ResourceNotFoundException("Event not found")));
 
 
         Asset asset = disposal.getAsset();
