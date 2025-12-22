481fee194b324a39da74ebb715a25dd9c73eb675
bala-amypo <bala@amypo.in>
2025-12-22 08:09:42 +0000

    User commit

--- src/main/java/com/example/demo/util/JwtUtil.java
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -12,7 +12,7 @@ public class JwtUtil {
     private static final String SECRET_KEY = "test-secret-key";
     private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
 
-    // ✅ MATCHES TEST SIGNATURE
+    
     public String generateToken(String username, Long userId, Set<String> roles) {
         return Jwts.builder()
                 .setSubject(username)
