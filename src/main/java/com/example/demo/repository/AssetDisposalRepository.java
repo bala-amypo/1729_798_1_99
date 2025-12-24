17c3ae7333c90d9e04caf980f32bd8172bfeb8b0
bala-amypo <bala@amypo.in>
2025-12-24 04:45:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetDisposalRepository.java
+++ src/main/java/com/example/demo/repository/AssetDisposalRepository.java
@@ -1,11 +1,20 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.AssetDisposal;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.Optional;
+
+// public interface AssetDisposalRepository extends JpaRepository<AssetDisposal, Long> {
+
+//     Optional<AssetDisposal> findByAssetId(Long assetId);
+// }
 
 import com.example.demo.entity.AssetDisposal;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import java.util.List;
 
 public interface AssetDisposalRepository extends JpaRepository<AssetDisposal, Long> {
-
-    Optional<AssetDisposal> findByAssetId(Long assetId);
+    List<AssetDisposal> findByApprovedBy(User user);
 }
