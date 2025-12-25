b4a541e509b09fb0f96579b068a2dd306724d096
bala-amypo <bala@amypo.in>
2025-12-25 10:21:35 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
@@ -1,8 +1,9 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.Vendor;
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.repository.VendorRepository;
+import com.example.demo.service.VendorService;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
